2e4d76625ee3e1a2a58f23e53f4054b5b66687e5
segye <tprpek@gmail.com>
2025-02-25 09:29:49 +0900

    refactor: voteService의 vote가 Long -> Page<SimilarResponse> 로 반환

--- src/main/kotlin/org/team14/webty/voting/controller/VoteController.kt
+++ src/main/kotlin/org/team14/webty/voting/controller/VoteController.kt
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.team14.webty.common.exception.BusinessException
 import org.team14.webty.common.exception.ErrorCode
+import org.team14.webty.common.mapper.PageMapper
 import org.team14.webty.security.authentication.WebtyUserDetails
 import org.team14.webty.voting.dto.VoteRequest
 import org.team14.webty.voting.service.VoteService
@@ -32,11 +33,14 @@ class VoteController(
         val webtyUserDetails = headerAccessor.sessionAttributes?.get("user") as? WebtyUserDetails
             ?: throw BusinessException(ErrorCode.USER_NOT_FOUND)
         val response =
-            voteService.vote(webtyUserDetails, voteRequest) // to do: voteService 에서 PageDto<SimilarResponse> 를 반환하도록 수정
+            voteService.vote(
+                webtyUserDetails,
+                voteRequest,
+            ) // to do: voteService 에서 PageDto<SimilarResponse> 를 반환하도록 수정
+        val responsePageDto = PageMapper.toPageDto(response)
         logger.info { "VoteService 실행 로그" }
-
         val destination = "/topic/vote-results/${voteRequest.similarId}"
-        simpMessagingTemplate.convertAndSend(destination, response)
+        simpMessagingTemplate.convertAndSend(destination, responsePageDto)
         logger.info { "WebSocket 메시지 전송 완료 - destination: $destination" }
     }
 
--- src/main/kotlin/org/team14/webty/voting/dto/VoteRequest.kt
+++ src/main/kotlin/org/team14/webty/voting/dto/VoteRequest.kt
@@ -2,5 +2,7 @@ package org.team14.webty.voting.dto
 
 data class VoteRequest(
     val similarId: Long,
-    val voteType: String
+    val voteType: String,
+    val page: Int = 0,
+    val size: Int = 10
 )
