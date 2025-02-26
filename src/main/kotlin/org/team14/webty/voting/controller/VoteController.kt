f296b589a34b96ba45349aeea118975abd08b0cd
segye <tprpek@gmail.com>
2025-02-26 09:26:19 +0900

    refactor: VoteController, VoteService 투표 취소 로직 수행 결과를 웹소켓을 통해 프론트에 전달하도록 했고 VoteController에서 기존에 VoteRequest에 simialrId와 voteType을 전달했었는데 투표 취소로직을 구현 하기 위해서는 similarId 값이 필요해 REST API 원칙을 준수하기 위해 similarId를 PathVaraible로 빼서 리팩토링 하였고 VoteService에서 중복 되는 코드를 리팩토링 하였습니다

--- src/main/kotlin/org/team14/webty/voting/controller/VoteController.kt
+++ src/main/kotlin/org/team14/webty/voting/controller/VoteController.kt
@@ -4,46 +4,40 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
-import org.team14.webty.common.mapper.PageMapper
-import org.team14.webty.common.redis.RedisPublisher
 import org.team14.webty.security.authentication.WebtyUserDetails
-import org.team14.webty.voting.dto.VoteRequest
 import org.team14.webty.voting.service.VoteService
 
 @RestController
 @RequestMapping("/vote")
 class VoteController(
     private val voteService: VoteService,
-    private val redisPublisher: RedisPublisher
 ) {
     private val logger = KotlinLogging.logger {}
 
     // 투표
-    @PostMapping
+    @PostMapping("/{similarId}")
     fun vote(
         @AuthenticationPrincipal webtyUserDetails: WebtyUserDetails,
-        @RequestBody voteRequest: VoteRequest,
+        @PathVariable(value = "similarId") similarId: Long,
+        @RequestParam(value = "voteType") voteType: String,
         @RequestParam(defaultValue = "0", value = "page") page: Int,
         @RequestParam(defaultValue = "10", value = "size") size: Int,
     ): ResponseEntity<Void> {
-        val response =
-            voteService.vote(webtyUserDetails, voteRequest, page, size)
-        val responsePageDto =
-            PageMapper.toPageDto(response) // to do: voteService 에서 PageDto<SimilarResponse> 를 반환하도록 수정
-        logger.info { "VoteService 실행 로그" }
-
-        redisPublisher.publish("vote-results", responsePageDto)
-
+        voteService.vote(webtyUserDetails, similarId, voteType, page, size)
+        logger.info { "VoteService 투표 실행 로그" }
         return ResponseEntity.ok().build() // 응답은 WebSocket통해서 받아오므로 상태값만 전달
     }
 
-    // 투표 취소 // to do: 웹소켓으로 변경
-    @DeleteMapping("/{voteId}")
+    // 투표 취소
+    @DeleteMapping("/{similarId}")
     fun cancel(
         @AuthenticationPrincipal webtyUserDetails: WebtyUserDetails,
-        @PathVariable(value = "voteId") voteId: Long
+        @PathVariable(value = "similarId") similarId: Long,
+        @RequestParam(defaultValue = "0", value = "page") page: Int,
+        @RequestParam(defaultValue = "10", value = "size") size: Int
     ): ResponseEntity<Void> {
-        voteService.cancel(webtyUserDetails, voteId)
-        return ResponseEntity.ok().build()
+        voteService.cancel(webtyUserDetails, similarId, page, size)
+        logger.info { "VoteService 투표 취소 로그" }
+        return ResponseEntity.ok().build() // 응답은 WebSocket통해서 받아오므로 상태값만 전달
     }
 }
--- src/main/kotlin/org/team14/webty/voting/service/VoteService.kt
+++ src/main/kotlin/org/team14/webty/voting/service/VoteService.kt
@@ -1,72 +1,66 @@
 package org.team14.webty.voting.service
 
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.team14.webty.common.exception.BusinessException
 import org.team14.webty.common.exception.ErrorCode
+import org.team14.webty.common.mapper.PageMapper
+import org.team14.webty.common.redis.RedisPublisher
 import org.team14.webty.security.authentication.AuthWebtyUserProvider
 import org.team14.webty.security.authentication.WebtyUserDetails
-import org.team14.webty.voting.dto.SimilarResponse
-import org.team14.webty.voting.dto.VoteRequest
 import org.team14.webty.voting.entity.Similar
-import org.team14.webty.voting.entity.Vote
 import org.team14.webty.voting.enums.VoteType
 import org.team14.webty.voting.mapper.SimilarMapper
 import org.team14.webty.voting.mapper.VoteMapper.toEntity
 import org.team14.webty.voting.repository.SimilarRepository
 import org.team14.webty.voting.repository.VoteRepository
 import org.team14.webty.webtoon.repository.WebtoonRepository
-import java.util.function.Supplier
 
 @Service
 class VoteService(
     private val voteRepository: VoteRepository,
     private val similarRepository: SimilarRepository,
     private val authWebtyUserProvider: AuthWebtyUserProvider,
-    private val webtoonRepository: WebtoonRepository
+    private val webtoonRepository: WebtoonRepository,
+    private val redisPublisher: RedisPublisher
 ) {
     // 유사 투표
     @Transactional
     fun vote(
         webtyUserDetails: WebtyUserDetails,
-        voteRequest: VoteRequest,
+        similarId: Long,
+        voteType: String,
         page: Int,
         size: Int
-    ): Page<SimilarResponse> {
+    ) {
         val pageable: Pageable = PageRequest.of(page, size)
         val webtyUser = authWebtyUserProvider.getAuthenticatedWebtyUser(webtyUserDetails)
-        val similar = similarRepository.findById(voteRequest.similarId)
+        val similar = similarRepository.findById(similarId)
             .orElseThrow { BusinessException(ErrorCode.SIMILAR_NOT_FOUND) }!!
         // 중복 투표 방지
         if (voteRepository.existsByUserIdAndSimilar(webtyUser.userId!!, similar)) {
             throw BusinessException(ErrorCode.VOTE_ALREADY_EXISTS)
         }
-        val vote = toEntity(webtyUser, similar, voteRequest.voteType)
+        val vote = toEntity(webtyUser, similar, voteType)
         voteRepository.save(vote)
         updateSimilarResult(similar)
-        val similars = similarRepository.findAllByTargetWebtoon(similar.targetWebtoon, pageable)
-
-        // to do: 투표 결과 계산 및 이에 따라 정렬하여 PageDto<SimilarResponse> 를 반환하도록 수정
-        return similars.map { mapSimilar: Similar ->
-            SimilarMapper.toResponse(
-                mapSimilar,
-                webtoonRepository.findById(mapSimilar.similarWebtoonId)
-                    .orElseThrow { BusinessException(ErrorCode.WEBTOON_NOT_FOUND) }
-            )
-        }
+        publish(similar, pageable)
     }
 
     // 투표 취소
     @Transactional
-    fun cancel(webtyUserDetails: WebtyUserDetails, voteId: Long) {
-        authWebtyUserProvider.getAuthenticatedWebtyUser(webtyUserDetails)
-        val vote: Vote = voteRepository.findById(voteId)
-            .orElseThrow(Supplier { BusinessException(ErrorCode.VOTE_NOT_FOUND) })
+    fun cancel(webtyUserDetails: WebtyUserDetails, similarId: Long, page: Int, size: Int) {
+        val pageable: Pageable = PageRequest.of(page, size)
+        val webtyUser = authWebtyUserProvider.getAuthenticatedWebtyUser(webtyUserDetails)
+        val similar =
+            similarRepository.findById(similarId).orElseThrow { BusinessException(ErrorCode.SIMILAR_NOT_FOUND) }
+        val vote = voteRepository.findBySimilarAndUserId(similar, webtyUser.userId!!)
+            .orElseThrow { BusinessException(ErrorCode.VOTE_NOT_FOUND) }
         voteRepository.delete(vote)
         updateSimilarResult(vote.similar)
+        publish(similar, pageable)
     }
 
     private fun updateSimilarResult(existingSimilar: Similar) {
@@ -77,5 +71,21 @@ class VoteService(
         // similarResult 업데이트
         val updateSimilar = existingSimilar.copy(similarResult = agreeCount - disagreeCount)
         similarRepository.save<Similar>(updateSimilar)
+    }
+
+    private fun publish(
+        similar: Similar,
+        pageable: Pageable
+    ) {
+        val similars = similarRepository.findAllByTargetWebtoon(similar.targetWebtoon, pageable)
+        val similarResponsePageDto = similars.map { mapSimilar: Similar ->
+            SimilarMapper.toResponse(
+                mapSimilar,
+                webtoonRepository.findById(mapSimilar.similarWebtoonId)
+                    .orElseThrow { BusinessException(ErrorCode.WEBTOON_NOT_FOUND) }
+            )
+        }.let { PageMapper.toPageDto(it) }
+
+        redisPublisher.publish("vote-results", similarResponsePageDto)
     }
 }
