f24aaa000c2243f0b4157b15db966bcd6a0d2847
segye <tprpek@gmail.com>
2025-02-26 12:13:13 +0900

    fix: 콤마 추가

--- src/main/kotlin/org/team14/webty/voting/dto/SimilarResponse.kt
+++ src/main/kotlin/org/team14/webty/voting/dto/SimilarResponse.kt
@@ -4,7 +4,7 @@ data class SimilarResponse(
     val similarId: Long,
     val similarThumbnailUrl: String,
     val similarResult: Long,
-    val similarWebtoonId: Long // webtoon-detail 페이지 이동 시 필요
+    val similarWebtoonId: Long, // webtoon-detail 페이지 이동 시 필요
     val agreeCount: Long,
     val disagreeCount: Long
 )
