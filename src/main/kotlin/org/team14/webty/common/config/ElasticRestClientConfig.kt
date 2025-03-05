1eb29e8259bd54e947c6018efef3a086cd3ce298
dia218 <dia19dev@gmail.com>
2025-03-06 01:20:07 +0900

    test: Refactor K6 DockerCompose & Fix ElasticSearch K6 Error

--- src/main/kotlin/org/team14/webty/common/config/ElasticRestClientConfig.kt
+++ src/main/kotlin/org/team14/webty/common/config/ElasticRestClientConfig.kt
@@ -1,5 +1,6 @@
 package org.team14.webty.common.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.elasticsearch.client.ClientConfiguration
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration
@@ -8,9 +9,14 @@ import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfigurat
 @Configuration
 class ElasticRestClientConfig : ElasticsearchConfiguration() {
 
+    @Value("\${spring.elasticsearch.uris}")
+    private lateinit var uris: String
+
     override fun clientConfiguration(): ClientConfiguration {
+        val formattedUris = uris.replace("http://", "").replace("https://", "") // 프로토콜 제거
+
         return ClientConfiguration.builder()
-            .connectedTo("localhost:9200")
+            .connectedTo(formattedUris)
             .build()
     }
 }
